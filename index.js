8221e867d78437f5e288e05141b8900811a1553c
pooh <cktjdgus45@naver.com>
2021-12-02 21:16:53 +0900

    typescript setting

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+var hi = function (name, age, gender) {
+    console.log("hello".concat(name, " im ").concat(age, "years old and ").concat(gender));
+};
+hi("paul", 25, "male");
---
+++ index.ts
@@ -0,0 +1,7 @@
+const hi = (name: string, age: number, gender: string) => {
+    console.log(`hello${name} im ${age}years old and ${gender}`);
+}
+
+hi("paul", 25, "male");
+
+export { };
