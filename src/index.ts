801d2e4ab2d280744410b181bb62812159a7a336
pooh <cktjdgus45@naver.com>
2021-12-03 12:42:55 +0900

    Creating Block

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,40 @@
-interface Human {
-   name: string,
-   age: number,
-   gender: string
+import * as CryptoJS from "crypto-js";
+
+class Block {
+   public index: number;
+   public hash: string;
+   public previousHash: string;
+   public data: string;
+   public timestamp: number;
+
+   static calculateBlockHash = (
+      index: number,
+      previousHash: string,
+      data: string,
+      timestamp: number
+   ): string => //
+      CryptoJS.SHA256(index + previousHash + data + timestamp).toString();
+
+   constructor(
+      index: number,
+      hash: string,
+      previousHash: string,
+      data: string,
+      timestamp: number
+   ) {
+      this.index = index;
+      this.hash = hash;
+      this.previousHash = previousHash;
+      this.data = data;
+      this.timestamp = timestamp;
+   }
 }
 
-const person = {
-   name: "cha",
-   age: 25,
-   gender: "male"
-}
+const genisisBlock: Block = new Block(0, "1111", "", "hello", 123456);
 
-const hi = (person: Human): string => {
-   return `hello im${person.name} ${person.age} years old and ${person.gender}`;
-}
+let blockchain: Block[] = [genisisBlock];
 
-console.log(hi(person));
+const getBlockchain = (): Block[] => blockchain;
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
 
-export { };
